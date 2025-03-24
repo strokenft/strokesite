fcbc0c2f371a4124903d025fce523baf3a62d433
IsaiahMars <isaiahmars10@gmail.com>
2025-03-24 15:23:28 -0400

    fifth commit :((

--- src/components/collectionPreview.js
+++ src/components/collectionPreview.js
@@ -1,50 +1,27 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import '../css/collectionPreview.css';
 
 const CollectionPreview = ({ nfts }) => {
-  const scrollRef = useRef(null);
-  
-  // Function to handle infinite scrolling effect
-  const handleScroll = () => {
-    const container = scrollRef.current;
-    const itemWidth = container.children[0].offsetWidth;
-    const scrollPosition = container.scrollLeft;
-    const containerWidth = container.clientWidth;
-    const totalScrollWidth = container.scrollWidth;
-    
-    // When the user reaches the end of the visible section of the first list, we jump to the end of the second set
-    if (scrollPosition + containerWidth >= totalScrollWidth - itemWidth) {
-      container.scrollLeft = itemWidth; // Jump to the start of the second list
-    }
-  };
-
-  useEffect(() => {
-    const container = scrollRef.current;
-    const interval = setInterval(() => {
-      container.scrollLeft += 1; // Auto-scroll to create the infinite effect
-    }, 20); // Adjust this value for scroll speed
-
-    // Clean up the interval on component unmount
-    return () => clearInterval(interval);
-  }, []);
-
   return (
     <div className="collectionPreview">
       <h2>Collection Preview</h2>
-      <div
-        ref={scrollRef}
-        className="nftList"
-        style={{ display: 'flex', overflowX: 'auto', scrollBehavior: 'smooth' }}
-        onScroll={handleScroll}
-      >
-        {/* Display the list twice for infinite scroll effect */}
-        {[...nfts, ...nfts].map((nft, index) => (
-          <div key={nft.identifier + index} className="nftItem" style={{ flexShrink: 0, marginRight: '15px' }}>
-            <a href={nft.opensea_url} target="_blank" rel="noopener noreferrer">
-              <img src={nft.image_url} alt={nft.name} />
-            </a>
-          </div>
-        ))}
+      <div className="scrollContainer">
+        <div className="scrollContent">
+          {[...nfts, ...nfts].map((nft, index) => (
+            <div
+              key={nft.identifier + index}
+              className="nftItem"
+            >
+              <a
+                href={nft.opensea_url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={nft.image_url} alt={nft.name} />
+              </a>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
